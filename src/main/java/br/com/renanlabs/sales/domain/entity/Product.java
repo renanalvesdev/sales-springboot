6e6e78dd7b023363e6c5e2d6545cce55bd33d7aa
Renan Alves <renanalvesemail@gmail.com>
2022-08-03 20:57:07 +0100

    starting with Spring Data JPA

--- src/main/java/br/com/renanlabs/sales/domain/entity/Product.java
+++ src/main/java/br/com/renanlabs/sales/domain/entity/Product.java
@@ -2,10 +2,27 @@ package br.com.renanlabs.sales.domain.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_product")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@Column(name = "unit_price")
 	private BigDecimal price;
 	
 	
