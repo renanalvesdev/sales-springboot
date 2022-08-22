d4ab44aa82276cd6a5eefbfd5712752895311d6d
Renan Alves <renanalvesemail@gmail.com>
2022-08-22 19:21:41 +0100

    restricting clients routes only for authenticated users

--- src/main/java/br/com/renanlabs/sales/config/SecurityConfig.java
+++ src/main/java/br/com/renanlabs/sales/config/SecurityConfig.java
@@ -31,8 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+			.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/api/clientes/**")
+					.authenticated()
+				.and()
+					.formLogin();
+						
 	}
 	
 }
